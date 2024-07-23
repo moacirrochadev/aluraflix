d4f2f76ee2b2fe9c8b0d739e7d449094a36ce7c2
moacirrochadev <empreendedormoacirneto@gmail.com>
2024-07-23 18:19:18 -0300

    atualizando tudo

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,13 +1,29 @@
-import logoFoto from "../../assets/logo.png";
+import logo from "../../assets/logo.png";
 
-export default function Header(){
+export function Header (){
+  function handleClick(){
+    alert("Você clicou no botão!");
+  }
+
     return (
-        <header className="flex items-center justify-between p-8 bg-zinc-900">
-        <img className="w-32" src={logoFoto} alt="" />
-        <nav className="flex gap-8">
-          <button className="text-zinc-200 text-2xl font-semibold p-2 border-4 border-blue-700">HOME</button>
-          <button className="w-46 text-zinc-200 text-2xl font-semibold p-2 border-4 border-blue-700">NOVO VÍDEO</button>
-        </nav>
-      </header>
-    )
+      <div>
+        <header className="flex items-center justify-between p-8">
+          <img src={logo} alt="Logo" />
+          <nav className="flex gap-8 border-4justify-between">
+            <button
+              onClick={handleClick}
+              className="text-zinc-200 text-2xl font-semibold w-[180px] p-2 border-4 border-blue-700 rounded-2xl"
+            >
+              HOME
+            </button>
+            <button
+              onClick={handleClick}
+              className="text-zinc-200 text-2xl font-semibold w-[180px] p-2 border-4 border-blue-700 rounded-2xl"
+            >
+              NOVO VÍDEO
+            </button>
+          </nav>
+        </header>
+      </div>
+    );
 }
---
+++ src/componentes/NewVideo/index.js
@@ -0,0 +1,7 @@
+export function NewVideo(){
+    return (
+        <>
+        <h1>Essa é outra página</h1>
+        </>
+    )
+}
