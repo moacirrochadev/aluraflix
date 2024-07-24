1dc660dbe969de75878db1df03f77bcb1269beb9
moacirrochadev <empreendedormoacirneto@gmail.com>
2024-07-23 23:16:14 -0300

    react router dom adicionado

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import { NewVideo } from "./componentes/NewVideo/index.js";
+import App from './App.js';
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App />,
+}, {
+  path: "/newvideo",
+  element: <NewVideo />
+}]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
